d345d9edaa08ff6f87b0da257d37e52f885d8331
Breno <brenluz@outlook.com>
2024-03-28 22:12:33 -0300

    exercicio 0.5

--- Chapter0/sketch.js
+++ Chapter0/sketch.js
@@ -3,13 +3,13 @@ let binho
 function setup() {
   createCanvas(400, 400);
   background(220);
-
+  let x = random(width);
+  let y = random(height);
+  binho = new Walker(x, y);
   }
   
   function draw() {
-    let x = randomGaussian(width/2, 10);
-    let y = randomGaussian(height/2, 10);
-    let color = randomGaussian(128,100);
-    fill(color);
-    circle(x,y,10);
+    binho.show();
+    binho.step();
+
   }
--- Chapter0/walker.js
+++ Chapter0/walker.js
@@ -9,4 +9,26 @@ class Walker {
         strokeWeight(5);
         point(this.x, this.y);
     }
+
+    step() {
+        
+        let xstep = randomGaussian(0, 5);
+        let ystep = randomGaussian(0, 5);
+
+        this.x += xstep;
+        this.y += ystep;
+
+        if (this.x > width) {
+            this.x = 0;
+        }
+        if (this.x < 0) {
+            this.x = width;
+        }
+        if (this.y > height) {
+            this.y = 0;
+        }
+        if (this.y < 0) {
+            this.y = height;
+        }
+}
 }
