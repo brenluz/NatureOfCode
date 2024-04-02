71e5844b56ea1539affa47c303d34e8840f5497d
Breno <brenluz@outlook.com>
2024-04-02 15:12:28 -0300

    exercise 0.7

---
+++ Chapter0/0.7/walker.js
@@ -0,0 +1,18 @@
+class Walker {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    show() {
+        stroke(0);
+        strokeWeight(5);
+        point(this.x, this.y);
+    }
+
+    step() {
+
+        this.x += noise(this.y) - 0.5;
+        this.y += noise(this.x) - 0.5;
+    }
+}
